00ca34e5282adafa09e2c032199916a7bf277e9f
DeyMass <silkin.mixail@yandex.ru>
2016-03-02 20:43:15 +0600

    Ispravil pochti vse!!!!!!!!!

--- Morskoy BoyNewBoats2.cpp
+++ Morskoy BoyNewBoats2.cpp
@@ -37,6 +37,29 @@ void turnprint(){
     
     }
 int pprint(){
+int t=565;
+   moveto(t,40);
+    outtext("0");
+    moveto(t,80);
+    outtext("1");
+    moveto(t,120);
+    outtext("2");
+    moveto(t,160);
+    outtext("3");
+    moveto(t,200);
+    outtext("4");
+    moveto(t,240);
+    outtext("5");
+    moveto(t,280);
+    outtext("6");
+    moveto(t,320);
+    outtext("7");
+    moveto(t,360);
+    outtext("8");
+    moveto(t,400);
+    outtext("9");
+    moveto(t,40);
+    outtext("1");
     for (int i=40;i<=440;i+=40){
     moveto(0,i);
     lineto(440,i);
@@ -54,7 +77,7 @@ int pprint(){
         moveto(i,0);
         lineto(i,440);
     }
-    int t=5;
+    t=5;
     settextstyle(2,0,16);
     moveto(t,80);
     outtext("1");
@@ -119,29 +142,6 @@ moveto(921,t);
 outtext("I");
 moveto(961,t);
 outtext("J");
-t=565;
-   moveto(t,40);
-    outtext("0");
-    moveto(t,80);
-    outtext("1");
-    moveto(t,120);
-    outtext("2");
-    moveto(t,160);
-    outtext("3");
-    moveto(t,200);
-    outtext("4");
-    moveto(t,240);
-    outtext("5");
-    moveto(t,280);
-    outtext("6");
-    moveto(t,320);
-    outtext("7");
-    moveto(t,360);
-    outtext("8");
-    moveto(t,400);
-    outtext("9");
-    moveto(t,40);
-    outtext("1");
     }
 
 
@@ -149,9 +149,9 @@ t=565;
 using namespace std;
 
 int main(){
-    int p1[11][11],p2[11][11],put=1;
-    for (int i=0;i<11;i++)
-    for (int j=0;j<11;j++){
+    int p1[12][12],p2[12][12],put=1;
+    for (int i=0;i<12;i++)
+    for (int j=0;j<12;j++){
 p1[i][j]=0;
 p2[i][j]=0;
 }
@@ -167,72 +167,66 @@ int krayx,krayy;
   setfillstyle(1,COLOR(0,255,0));
   //*********************************************************×ÅÒÛÐ¨ÕÏÀËÓÁÍÈÊ*************************************************************
 while (1){
-    if (first==1) printf("FIRST\n");
-    if (second==1) printf("second\n");
-    if (third==1) printf("THIRD\n");
          if (ismouseclick(WM_LBUTTONDOWN)&&mousex()>40&&mousex()<440&&mousey()>40&&mousey()<440&&mousex()%40!=0&&mousey()%40!=0){
        if (first){
-            if (p1[(int)(((float)mousex()-40)/40)][(int)(((float)mousey()-40)/40)]<1&&mousex()%40!=0&&mousey()%40!=0){
-            p1[(int)(((float)mousex()-40)/40)][(int)(((float)mousey()-40)/40)]=41;
+            if (p1[(int)(((float)mousex())/40)][(int)(((float)mousey())/40)]<1&&mousex()%40!=0&&mousey()%40!=0){
+            p1[(int)(((float)mousex())/40)][(int)(((float)mousey())/40)]=41;
           floodfill(mousex(),mousey(),COLOR(255,255,255));    
           second=1;
           first=0;    
-          ex=(int)(((float)mousex()-40)/40);
-          ey=(int)(((float)mousey()-40)/40);
+          ex=(int)((float)mousex()/40);
+          ey=(int)((float)mousey()/40);
             }
          }
       
       
       if (first==0)
          if (second){
- if ((p1[(int)(((float)mousex()-40)/40)+1][(int)(((float)mousey()-40)/40)]==41||p1[(int)(((float)mousex()-40)/40)][(int)(((float)mousey()-40)/40)+1]==41
- ||p1[(int)(((float)mousex()-40)/40)-1][(int)(((float)mousey()-40)/40)]==41
- ||p1[(int)(((float)mousex()-40)/40)][(int)(((float)mousey()-40)/40)-1]==41)&&p1[(int)(((float)mousex()-40)/40)][(int)(((float)mousey()-40)/40)]<1){
+ if ((p1[(int)((float)mousex()/40)+1][(int)((float)mousey()/40)]==41||p1[(int)((float)mousex()/40)][(int)((float)mousey()/40)+1]==41
+ ||p1[(int)((float)mousex()/40)-1][(int)((float)mousey()/40)]==41
+ ||p1[(int)((float)mousex()/40)][(int)((float)mousey()/40)-1]==41)&&p1[(int)((float)mousex()/40)][(int)((float)mousey()/40)]<1){
   floodfill(mousex(),mousey(),COLOR(255,255,255));
-  p1[(int)(((float)mousex()-40)/40)][(int)(((float)mousey()-40)/40)]=41;
+  p1[(int)((float)mousex()/40)][(int)((float)mousey()/40)]=41;
   second=0;
   third=2;  
 
-  if (ex-((int)(((float)mousex()-40)/40))==0) dir=2;
+  if (ex-((int)((float)mousex()/40))==0) dir=2;
   else dir=1;
   printf("dir=%i",dir);
  }}
             if (third>=0){
              if(dir==2){
-              if ((p1[(int)(((float)mousex()-40)/40)][(int)(((float)mousey()-40)/40)+1]==41||p1[(int)(((float)mousex()-40)/40)][(int)(((float)mousey()-40)/40)-1]==41)&&p1[(int)(((float)mousex()-40)/40)][(int)(((float)mousey()-40)/40)]<1){
+              if ((p1[(int)((float)mousex()/40)][(int)((float)mousey()/40)+1]==41||p1[(int)((float)mousex()/40)][(int)((float)mousey()/40)-1]==41)&&p1[(int)((float)mousex()/40)][(int)((float)mousey()/40)]<1){
                     third--;
   floodfill(mousex(),mousey(),COLOR(255,255,255));
-  p1[(int)(((float)mousex()-40)/40)][(int)(((float)mousey()-40)/40)]=41;
+  p1[(int)((float)mousex()/40)][(int)((float)mousey()/40)]=41;
      if (third==0) break;
 }
              }         
              if (dir==1){
-        if (p1[(int)(((float)mousex()-40)/40)+1][(int)(((float)mousey()-40)/40)]>1||p1[(int)(((float)mousex()-40)/40)-1][(int)(((float)mousey()-40)/40)]>1){
+        if (p1[(int)((float)mousex()/40)+1][(int)((float)mousey()/40)]>1||p1[(int)((float)mousex()/40)-1][(int)((float)mousey()/40)]>1){
                             third--;
   floodfill(mousex(),mousey(),COLOR(255,255,255));
-  p1[(int)(((float)mousex()-40)/40)][(int)(((float)mousey()-40)/40)]=41;
+  p1[(int)((float)mousex()/40)][(int)((float)mousey()/40)]=41;
                     }    
                 }         
             }
         if (third==0) break;
 }//*******************************************
       clearmouseclick(WM_LBUTTONDOWN);
-      delay(100);
+      delay(10);
 }
 bgiout<<"Ïîñòàâüòå äâà 3-õ ïàëóáíèêà";
 outstreamxy(0,700);
 
-int n=2,tempx,tempy;
+int n=1,tempx,tempy;
 first=1;
 setfillstyle(1,COLOR(0,255,0));
 dir=0;
 while (1){
-if (n==1){bgiout<<"Ïîñòàâüòå 3-õ ïàëóáíèê";
-outstreamxy(0,700);
-}
-clearmouseclick(WM_LBUTTONDOWN);
-    tempx=((int)(float)mousex()-40)/40;
-    tempy=((int)(float)mousey()-40)/40;
+
+    tempx=(int)((float)mousex()/40);
+    tempy=(int)((float)mousey()/40);
     if (ismouseclick(WM_LBUTTONDOWN)&&mousex()>40&&mousey()>40&&mousex()%40!=0&&mousey()%40!=0){
         if (first==1&&p1[tempx][tempy]<1&&p1[tempx][tempy+1]<1&&p1[tempx+1][tempy]<1&&p1[tempx+1][tempy+1]<1&&p1[tempx-1][tempy]<1&&p1[tempx][tempy-1]<1&&p1[tempx-1][tempy-1]<1&&p1[tempx-1][tempy+1]<1&&p1[tempx+1][tempy-1]<1){
             p1[tempx][tempy]=31;
@@ -241,7 +235,7 @@ clearmouseclick(WM_LBUTTONDOWN);
             second=1;
             clearmouseclick(WM_LBUTTONDOWN);
             }
-        if (second==1&&(p1[tempx][tempy+1]==31||p1[tempx][tempy-1]==31)&&p1[tempx][tempy]<1&&p1[tempx+1][tempy+1]<1&&p1[tempx-1][tempy-1]<1&&p1[tempx-1][tempy+1]<1&&p1[tempx+1][tempy-1]<1){
+        if (second==1&&(p1[tempx-1][tempy]==31||p1[tempx+1][tempy]==31||p1[tempx][tempy+1]==31||p1[tempx][tempy-1]==32)&&p1[tempx][tempy]<1&&p1[tempx+1][tempy+1]<1&&p1[tempx-1][tempy-1]<1&&p1[tempx-1][tempy+1]<1&&p1[tempx+1][tempy-1]<1){
             if (p1[tempx][tempy+1]==31||p1[tempx][tempy-1]==31) dir=2;
             else dir=1;
             p1[tempx][tempy]=31;
@@ -267,12 +261,109 @@ clearmouseclick(WM_LBUTTONDOWN);
             }
         }
     if (n==0) break;
+    clearmouseclick(WM_LBUTTONDOWN);
+    delay(1);
+    }
+n=1;
+first=1;
+setfillstyle(1,COLOR(0,255,0));
+dir=0;
+bgiout<<"ïîñòàâüòå 3-õ ïàëóáíèê";
+outstreamxy(0,700);
+while (1){
+
+    tempx=(int)((float)mousex()/40);
+    tempy=(int)((float)mousey()/40);
+    if (ismouseclick(WM_LBUTTONDOWN)&&mousex()>40&&mousey()>40&&mousex()%40!=0&&mousey()%40!=0){
+        if (first==1&&p1[tempx][tempy]<1&&p1[tempx][tempy+1]<1&&p1[tempx+1][tempy]<1&&p1[tempx+1][tempy+1]<1&&p1[tempx-1][tempy]<1&&p1[tempx][tempy-1]<1&&p1[tempx-1][tempy-1]<1&&p1[tempx-1][tempy+1]<1&&p1[tempx+1][tempy-1]<1){
+            p1[tempx][tempy]=32;
+            floodfill(mousex(),mousey(),COLOR(255,255,255));
+            first=0;
+            second=1;
+            clearmouseclick(WM_LBUTTONDOWN);    
+            }
+        if (second==1&&(p1[tempx-1][tempy]==32||p1[tempx+1][tempy]==32||p1[tempx][tempy+1]==32||p1[tempx][tempy-1]==32)&&p1[tempx][tempy]<1&&p1[tempx+1][tempy+1]<1&&p1[tempx-1][tempy-1]<1&&p1[tempx-1][tempy+1]<1&&p1[tempx+1][tempy-1]<1){
+            if (p1[tempx][tempy+1]==32||p1[tempx][tempy-1]==32) dir=2;
+            else dir=1;
+            p1[tempx][tempy]=32;
+            floodfill(mousex(),mousey(),COLOR(255,255,255));
+            second=0;
+            third=1;
+            }
+        if (third==1&&dir==2&&p1[tempx+1][tempy+1]<1&&p1[tempx-1][tempy-1]<1&&p1[tempx+1][tempy-1]<1&&p1[tempx-1][tempy+1]<1&&p1[tempx][tempy]<1&&((p1[tempx][tempy-1]==32&&p1[tempx][tempy+1]==0)||(p1[tempx][tempy-1]==0&&p1[tempx][tempy+1]==32))){
+            p1[tempx][tempy]=32;
+            floodfill(mousex(),mousey(),COLOR(255,255,255));
+            n--;
+            first=1;
+            third=0;
+            dir=0;
+            }
+        if (third==1&&dir==1&&p1[tempx+1][tempy+1]<1&&p1[tempx-1][tempy-1]<1&&p1[tempx+1][tempy-1]<1&&p1[tempx-1][tempy+1]<1&&p1[tempx][tempy]<1&&((p1[tempx-1][tempy]==0&&p1[tempx+1][tempy]==32)||(p1[tempx-1][tempy]==32&&p1[tempx+1][tempy]==0))){
+            p1[tempx][tempy]=32;
+            floodfill(mousex(),mousey(),COLOR(255,255,255));
+            n--;
+            first=1;
+            third=0;
+            dir=0;
+            }
+        }
+    if (n==0) break;
+    
+clearmouseclick(WM_LBUTTONDOWN);
+    delay(1);
     }
 
-
-
-
-
+bgiout<<"ïîñòàâüòå 3 2-õ ïàëóáíèêà";
+outstreamxy(0,700);
+n=3;
+first=1;
+third=0;
+second=0;
+//---------------------------------------------------ÄÂÓÕÏÀËÓÁÍÈÊÈ! ÎÄÍÈÌ ÖÈÊËÎÌ---------------------------------------------------------------------
+while(n>0){
+    tempx=(int)((float)mousex()/40);
+    tempy=(int)((float)mousey()/40);
+    
+    if (ismouseclick(WM_LBUTTONDOWN)&&mousex()>40&&mousey()>40&&mousex()%40!=0&&mousey()%40!=0){
+        if (first==1&&p1[tempx][tempy]<1&&p1[tempx][tempy+1]<1&&p1[tempx+1][tempy]<1&&p1[tempx+1][tempy+1]<1&&p1[tempx-1][tempy]<1&&p1[tempx][tempy-1]<1&&p1[tempx-1][tempy-1]<1&&p1[tempx-1][tempy+1]<1&&p1[tempx+1][tempy-1]<1){
+            p1[tempx][tempy]=20+n;
+            floodfill(mousex(),mousey(),COLOR(255,255,255));
+            first=0;
+            second=1;
+            }
+        if (second==1&&p1[tempx][tempy]<1&&p1[tempx+1][tempy+1]<1&&p1[tempx-1][tempy-1]<1&&p1[tempx+1][tempy-1]<1&&p1[tempx-1][tempy+1]<1&&(p1[tempx-1][tempy]==20+n||p1[tempx+1][tempy]==20+n||p1[tempx][tempy+1]==20+n||p1[tempx][tempy-1]==20+n)){
+            p1[tempx][tempy]=20+n;
+            floodfill(mousex(),mousey(),COLOR(255,255,255));
+            second=0;
+            n--;
+            first=1;
+            }
+    }
+    printf("%i\n",p1[tempx][tempy]);
+    clearmouseclick(WM_LBUTTONDOWN);    
+    delay(5);
+}
+n=4;
+first=1;
+second=0;
+third=0;
+bgiout<<"Ïîñòàâüòå 4 îäíîïàëóáíèêà";
+outstreamxy(0,700);
+while (n>0){
+    tempx=(int)((float)mousex()/40);
+    tempy=(int)((float)mousey()/40);
+    
+    if (ismouseclick(WM_LBUTTONDOWN)&&mousex()>40&&mousey()>40&&mousex()%40!=0&&mousey()%40!=0){
+        if (p1[tempx][tempy]<1&&p1[tempx][tempy+1]<1&&p1[tempx+1][tempy]<1&&p1[tempx+1][tempy+1]<1&&p1[tempx-1][tempy]<1&&p1[tempx][tempy-1]<1&&p1[tempx-1][tempy-1]<1&&p1[tempx-1][tempy+1]<1&&p1[tempx+1][tempy-1]<1){
+            p1[tempx][tempy]=10+n;
+            floodfill(mousex(),mousey(),COLOR(255,255,255));
+            n--;
+        }
+    }
+    clearmouseclick(WM_LBUTTONDOWN);
+    delay(5);
+    
+}
 
 
 
